0f71a76f90daf71b4536869251e76af461e8568b
MaStErMiNd012 <66347386+MaStErMiNd012@users.noreply.github.com>
2025-08-26 18:10:46 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,14 @@
+#task 1
+
+enter_the_first_number = int(input("Enter a first number: "))
+enter_the_second_number = int(input("Enter a second number: "))
+print("addition: ", enter_the_first_number + enter_the_second_number)
+print("subtraction: ", enter_the_first_number - enter_the_second_number)
+print("multiplication: ",  enter_the_first_number * enter_the_second_number)
+print("division: " , enter_the_first_number / enter_the_second_number)
+
+#task 2 (greetings)
+
+enter_your_first_name = input("Enter your first name: ")
+enter_your_last_name = input("Enter your last name: ")
+print ("hello", enter_your_first_name + enter_your_last_name,"!"+"\twelcome to the python program.")
